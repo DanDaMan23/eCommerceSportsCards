fd4e91a00f8bc527f002e4952fa2c58c203a2660
DanDaMan23 <danlawrcons@gmail.com>
2020-11-12 01:47:27 -0600

    Created Post model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :player
   belongs_to :team
-
+  validates :price, :quantity, :player, :team, presence: true
   has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_043159) do
+ActiveRecord::Schema.define(version: 2020_11_12_074652) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 2020_11_12_043159) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
