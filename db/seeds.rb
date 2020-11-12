286b927d706199466362cec688bd44fcd4365d58
DanDaMan23 <danlawrcons@gmail.com>
2020-11-11 18:11:11 -0600

    Seed Script Created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Card.delete_all
+Player.delete_all
+Team.delete_all
+
+def create_players(number_of_players)
+  number_of_players.times do
+    player = Faker::Sports::Basketball.unique.player
+    Player.create(name: player)
+  end
+end
+
+def create_teams(number_of_team)
+  number_of_team.times do
+    team = Faker::Sports::Basketball.unique.team
+    team = team.split(' ')
+    Team.create(name: team[-1], city: team[0])
+  end
+end
+
+create_players(20)
+create_teams(30)
+
+puts "Players created: #{Player.count}"
+puts "Teams created #{Team.count}"
+puts "Cards created: #{Card.count}"
