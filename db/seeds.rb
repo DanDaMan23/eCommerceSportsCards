983a77eab9a8e7574257fab793a9947c759a5fe8
DanDaMan23 <danlawrcons@gmail.com>
2020-11-18 16:39:05 -0600

    Updated Seed Script

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,15 +24,30 @@ def create_teams
 
 end
 
+def create_cards(number_of_cards)
+  card_url = "https://nba-players.herokuapp.com/players-stats"
+  response = RestClient.get(card_url)
+
+  cards = JSON.parse(response)
+
+  (0..number_of_cards).each do |i|
+    player = Player.find_or_create_by(name: "#{cards[i]["name"]}")
+    team = Team.where("name LIKE '#{cards[i]["team_name"].split(' ')[-1]}'")
+    Card.create(price: 10, quantity: 2, brand: "No Name", player: player, team: team[0])
+  end
+
+end
+
 create_players(20)
 create_teams
+create_cards(110)
 
 puts "Players created: #{Player.count}"
 puts "Teams created #{Team.count}"
 puts "Cards created: #{Card.count}"
 
-if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-end
+# if Rails.env.development?
+#   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# end
 
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
