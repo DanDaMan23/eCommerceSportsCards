9e01df3b14cf9c1e119a5f1154686534f4a658f3
DanDaMan23 <danlawrcons@gmail.com>
2020-11-25 03:47:07 -0600

    Dropdown Implemented

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,25 +1,29 @@
 class CardsController < ApplicationController
+  
   def index
+    @teams = Team.all().order(:name)
     if params[:search]
-      # @cards = Card.where("brand LIKE '#{params[:search]}%'").page(params[:page])
-      @cards = Card.joins(:player).where("name LIKE '#{params[:search]}%'").page(params[:page])
+      @cards = Card.joins(:player, :team).where("players.name LIKE '#{params[:search]}%' AND teams.id = #{params[:choice]}").page(params[:page])
     else
       @cards = Card.all.page(params[:page])
     end
   end
 
   def new_cards
+    @teams = Team.all().order(:name)
     if params[:search]
-      @cards = Card.joins(:player).where("name LIKE ? AND cards.created_at > ?", "#{params[:search]}%", Date.today - 3).page(params[:page])
+      @cards = Card.joins(:player, :team).where("players.name LIKE ? AND teams.id = ? AND cards.created_at > ?",
+        "#{params[:search]}%", params[:choice], Date.today - 3).page(params[:page])
     else
       @cards = Card.all.where("created_at > ?", Date.today - 6).page(params[:page])
     end
   end
 
   def updated_cards
+    @teams = Team.all().order(:name)
     if params[:search]
-      @cards = Card.joins(:player).where("name LIKE ? AND cards.updated_at > ? AND cards.updated_at <> cards.created_at",
-        "#{params[:search]}%", Date.today - 3).page(params[:page])
+      @cards = Card.joins(:player, :team).where("players.name LIKE ? AND teams.id = ? AND cards.updated_at > ? AND cards.updated_at <> cards.created_at",
+        "#{params[:search]}%", params[:choice], Date.today - 3).page(params[:page])
     else
       @cards = Card.all.where("updated_at > ? AND updated_at <> created_at", Date.today - 3).page(params[:page])
     end
