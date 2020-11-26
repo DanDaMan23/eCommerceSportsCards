35751b0a38d3378cb28fb376d0029a13ee9a29e8
DanDaMan23 <danlawrcons@gmail.com>
2020-11-25 21:15:21 -0600

    Added remoce feature in the cart

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -54,8 +54,13 @@ class CardsController < ApplicationController
 
   def add_to_cart
     id = params[:id].to_i
-
     session[:cart] << id unless session[:cart].include?(id)
+    redirect_to cards_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
     redirect_to cards_path
   end
 
